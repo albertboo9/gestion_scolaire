266e3ff5441bce6e2a63df5286d17f4ed777bfc1
albertboo9 <booalbert60@gmail.com>
2025-01-27 13:38:22 +0100

    début  de l'intégration

--- stunet/src/components/HeaderParents.js
+++ stunet/src/components/HeaderParents.js
@@ -10,10 +10,10 @@ const HeaderParents = () => {
         <img src={logo} alt="logo" />
       </div>
       <div className='liens'>
-        <Link to="/" className='lien'>
+        <Link to="/home/gestionparent/Notifications" className='lien'>
           <BiBell size={30}/>Notifications</Link>
-        <Link to="/Messages" className='lien'><BiEnvelope size={30}/>Messagess</Link>
-        <Link to="/Assiduite" className='lien'><BiUserCheck size={30}/>SuiviEleve</Link>
+        <Link to="/home/gestionparent/Messages" className='lien'><BiEnvelope size={30}/>Messagess</Link>
+        <Link to="/home/gestionparent/Assiduite" className='lien'><BiUserCheck size={30}/>SuiviEleve</Link>
       </div>
         
     </nav>
